e735c1321f64a9b0f650407489d5012e0b4fbfbd
kloo <macbookpro@MacBook-Pro-de-MacBookPro.local>
2015-04-15 00:22:39 +0200

    quelques modifications dans les mesh des iles, ajout d'une lampe de poche(click gauche), le curseur de la sourie est maintenant verrouillé au centre de l'écran et est invisible

---
+++ TheFisher/Assets/LightControler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightControler : MonoBehaviour 
+{
+
+	private Light onOff;
+	private bool isOn;
+	
+	void Start () 
+	{
+		onOff = GetComponent <Light>();
+		isOn = true;
+		
+		Screen.lockCursor = isOn; // Lock the mouse's cursor at the middle of the screen
+		Cursor.visible = false; // It means what is written.
+	}
+
+	void LateUpdate () 
+	{
+		if (isOn)
+		{
+			onOff.enabled = true; // Light On
+
+			if (Input.GetMouseButtonDown(0)) // On Left click turn the light Off
+				isOn = false;
+
+		}
+		else
+		{
+			onOff.enabled = false; // Light Off
+
+			if (Input.GetMouseButtonDown(0)) // On Left click turn the light On
+				isOn = true;
+		}
+
+	}
+}
